5c56f90e46acfc80922b2da6e11b2e06b41c3526
Mikxiz <38124633+Mikxiz@users.noreply.github.com>
2019-04-10 12:02:29 +0300

    Lisätty yksinkertainen toteutus laitteiden tallentamisesta tiettyyn paikkaan.

---
+++ AssetManagementWeb/AssetManagementWeb/Models/AssignLocationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AssetManagementWeb.Models
+{
+    public class AssignLocationModel
+    {
+        public string MyProperty { get; set; }
+        public string LocationCode { get; set; }
+    }
+}
