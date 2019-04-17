18bbb6c73f0b36aeb142ba220d2bb35acd2848ea
Mikxiz <mika.kilpia@gmail.com>
2019-04-17 08:36:00 +0300

    Lisätty TypeScript-tuki projektiin ja samalla aloitettu JSON-testisivun toteutus.

--- AssetManagementWeb/AssetManagementWeb/Scripts/Application/Logic.js
+++ AssetManagementWeb/AssetManagementWeb/Scripts/Application/Logic.js
@@ -1,3 +1,4 @@
+/// <reference path="../typings/jquery/jquery.d.ts" />
 function initAssetAssignment() {
     $("#AssignAssetButton").click(function () {
         alert("Toimii!");
--- AssetManagementWeb/AssetManagementWeb/Scripts/Application/Logic.ts
+++ AssetManagementWeb/AssetManagementWeb/Scripts/Application/Logic.ts
@@ -1,4 +1,6 @@
-﻿function initAssetAssignment() {
+﻿/// <reference path="../typings/jquery/jquery.d.ts" />
+
+function initAssetAssignment() {
 	$("#AssignAssetButton").click(function () {
 		alert("Toimii!");
 	});
